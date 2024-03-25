58b1ab7927e622337df46acfab0a0dc3ab87cfde
Luiz Eduardo <146301535+LuizEdu-AR@users.noreply.github.com>
2024-03-25 15:36:45 -0300

    Implementação da função para coletar os dados dos pacientes

--- Código C/src/paciente.c
+++ Código C/src/paciente.c
@@ -9,3 +9,33 @@ struct paciente{
     char enfermidade[100];
     char historico_medico[100];
 };
+
+void coletarDadosPaciente(Paciente* paciente){
+
+    FILE* arquivo = fopen(ArquivoPaciente, "a");
+    
+    if(arquivo == NULL){
+        printf("Erro ao abrir o arquivo\n");
+        exit(1);
+    }
+
+    printf("Digite o nome do paciente: ");
+    fgets(paciente->nome, sizeof(paciente->nome), stdin);
+    paciente->nome[strcspn(paciente->nome, "\n")] = '\0';
+
+    printf("Digite a idade do paciente: ");
+    scanf("%d", &paciente->idade);
+
+    //limpa o buffer do teclado
+    while (getchar() != '\n');
+
+    printf("Digite a enfermidade do paciente: ");
+    fgets(paciente->enfermidade, sizeof(paciente->enfermidade), stdin);
+    paciente->enfermidade[strcspn(paciente->enfermidade, "\n")] = '\0';
+
+    printf("Digite o historico medico do paciente: ");
+    fgets(paciente->historico_medico, sizeof(paciente->historico_medico), stdin);
+    paciente->historico_medico[strcspn(paciente->historico_medico, "\n")] = '\0';
+
+    fclose(arquivo);
+}
