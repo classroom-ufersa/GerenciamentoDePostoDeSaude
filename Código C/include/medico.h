98feb4336987e0e9c6e1f2ee2f8964167e0ffff2
Everson-Alisson <eversonalisson365@gmail.com>
2024-04-15 10:09:23 -0300

    adicionando os cabeçalhos das funções de pacientes

--- Código C/include/medico.h
+++ Código C/include/medico.h
@@ -1,10 +1,1 @@
-#define ArquivoMedico "listademedicos.txt"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-//Estrutura para medicos 
-typedef struct medico Medico;
-
-//Função para registrar medicos
-void RegistrarMedico(Medico* medico);
--- Código C/include/paciente.h
+++ Código C/include/paciente.h
@@ -1,10 +1,36 @@
 #define ArquivoPaciente "listadepacientes.txt"
+#include "funcoes.h"
+#include "medico.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h> 
+#include <ctype.h>
+
+#define MAX_CARACTER 300
 
 // Estrutura do paciente
-typedef struct paciente Paciente;
+typedef struct paciente Paciente; // Fix struct name inconsistency
 
-//Função para coletar os dados dos pacientes
-void AdicionarPaciente(Paciente* paciente);
+// Estrutura da lista de pacientes
+typedef struct listaPaciente ListaPaciente;
+
+// Função para criar lista de pacientes
+void CriaListaPaciente(ListaPaciente **lista);
+
+// Função para adicionar paciente a algum medico
+void AdicionaPaciente(ListaPaciente **lista, Paciente *paciente);
+
+// Função para remover paciente de algum medico
+void RemovePaciente(ListaPaciente **lista, char *nome);
+
+// Função para editar informações do paciente
+void EditarInformacoesDoPaciente(ListaPaciente **lista);
+
+//Função para buscar pacientes
+void BuscarPaciente(ListaPaciente **lista);
+
+// Função para listar os medicos e seus pacientes
+void Listarmedico(ListaMedico **lista);
+
+// Função para pegar os dados do arquivo passar para a lista
+void NovoPacienteTxt(ListaPaciente **lista, char *nome, int idade, char *enfermidade, char *historico_medico);
